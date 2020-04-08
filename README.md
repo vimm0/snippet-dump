62bd679b459324737b489d109e07266e2f5ea888
vimm0 <vimmrana0@gmail.com>
2020-04-08 17:18:48 +0545

    Add readme and ignore file

---
+++ README.md
@@ -0,0 +1,5 @@
+# Script dump
+> Dump any snippet here
+
+#### Reference
+- Python 3 Object Oriented Programming Harness the power of Python 3 objects
--- python/example1.py
+++ python/example1.py
@@ -1,17 +1,19 @@
 import math
 
+
 class Point:
     def move(self, x, y):
-	self.x = x
+        self.x = x
         self.y = y
 
     def reset(self):
-	self.move(0, 0)
+        self.move(0, 0)
 
     def calculate_distance(self, other_point):
-	return math.sqrt(
-		(self.x - other_point.x)**2 +
-		(self.y - other_point.y)**2)
+        return math.sqrt(
+            (self.x - other_point.x) ** 2 +
+            (self.y - other_point.y) ** 2)
+
 
 # how to use it
 point1 = Point()
@@ -26,3 +28,9 @@ assert (point2.calculate_distance(point1) == point1.calculate_distance(point2))
 point1.move(3, 4)
 print(point1.calculate_distance(point2))
 print(point1.calculate_distance(point1))
+
+# OUTPUT
+# =======
+# 5.0
+# 4.47213595499958
+# 0.0
